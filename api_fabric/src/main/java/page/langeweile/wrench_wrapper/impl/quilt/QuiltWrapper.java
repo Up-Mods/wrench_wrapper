5784df16b80db6e0695a3725411ae3e2155dab7c
Ennui Langeweile <85590273+ennuil@users.noreply.github.com>
2025-11-21 18:23:23 -0300

    Erase TOML, cease using Quilt Config's ConfigEnvironment

--- api_fabric/src/main/java/page/langeweile/wrench_wrapper/impl/quilt/QuiltWrapper.java
+++ api_fabric/src/main/java/page/langeweile/wrench_wrapper/impl/quilt/QuiltWrapper.java
@@ -1,16 +1,19 @@
 package page.langeweile.wrench_wrapper.impl.quilt;
 
 import org.quiltmc.config.api.ReflectiveConfig;
+import org.quiltmc.config.api.serializers.Json5Serializer;
 import org.quiltmc.config.implementor_api.ConfigEnvironment;
-import org.quiltmc.loader.api.config.v2.QuiltConfig;
-import org.quiltmc.loader.impl.config.QuiltConfigImpl;
+import org.quiltmc.config.implementor_api.ConfigFactory;
+import org.quiltmc.loader.api.QuiltLoader;
 
 public class QuiltWrapper {
+	private static final ConfigEnvironment CONFIG_ENVIRONMENT = new ConfigEnvironment(QuiltLoader.getConfigDir(), Json5Serializer.INSTANCE, Json5Serializer.INSTANCE);
+
 	public static <C extends ReflectiveConfig> C create(String family, String id, Class<C> configCreatorClass) {
-		return QuiltConfig.create(family, id, configCreatorClass, builder -> builder.format("json5"));
+		return ConfigFactory.create(CONFIG_ENVIRONMENT, family, id, configCreatorClass);
 	}
 
 	public static ConfigEnvironment getConfigEnvironment() {
-		return QuiltConfigImpl.getConfigEnvironment();
+		return CONFIG_ENVIRONMENT;
 	}
 }
--- api_neoforge/src/main/java/page/langeweile/wrench_wrapper/impl/norge/NorgeWrapper.java
+++ api_neoforge/src/main/java/page/langeweile/wrench_wrapper/impl/norge/NorgeWrapper.java
@@ -2,13 +2,12 @@ package page.langeweile.wrench_wrapper.impl.norge;
 
 import net.neoforged.fml.loading.FMLPaths;
 import org.quiltmc.config.api.ReflectiveConfig;
-import org.quiltmc.config.api.serializers.TomlSerializer;
+import org.quiltmc.config.api.serializers.Json5Serializer;
 import org.quiltmc.config.implementor_api.ConfigEnvironment;
 import org.quiltmc.config.implementor_api.ConfigFactory;
 
 public class NorgeWrapper {
-	@SuppressWarnings("deprecation")
-	private static final ConfigEnvironment CONFIG_ENVIRONMENT = new ConfigEnvironment(FMLPaths.CONFIGDIR.get(), TomlSerializer.INSTANCE, TomlSerializer.INSTANCE);
+	private static final ConfigEnvironment CONFIG_ENVIRONMENT = new ConfigEnvironment(FMLPaths.CONFIGDIR.get(), Json5Serializer.INSTANCE, Json5Serializer.INSTANCE);
 
 	public static <C extends ReflectiveConfig> C create(String family, String id, Class<C> configCreatorClass) {
 		return ConfigFactory.create(CONFIG_ENVIRONMENT, family, id, configCreatorClass);
